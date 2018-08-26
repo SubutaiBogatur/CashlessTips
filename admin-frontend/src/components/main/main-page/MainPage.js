3e86fcd004e9dd869ea04823cac0f877fc07f73e
evarand <artyom.shalagin@gmail.com>
2018-08-27 00:03:58 +0300

    main page - added main card number

--- admin-frontend/src/components/main/main-page/MainPage.js
+++ admin-frontend/src/components/main/main-page/MainPage.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import {Paper, Typography, Button} from '@material-ui/core';
+import {Paper, Typography, Button, Divider, TextField} from '@material-ui/core';
 import './MainPage.css';
 
 export class MainPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            cardNumber: null
         }
+    }
+
+    componentWillMount() {
+        this.setState({
+            cardNumber: this.loadData().cardNumber
+        })
+    }
+
+    loadData() {
+        // TODO
+        return {
+            cardNumber: null
+        }
+    }
+
+    saveCard() {
+        alert('actual connection to server is wip')
     }
 
     render() {
@@ -28,12 +45,35 @@ export class MainPage extends Component {
                                 оставлены для каждого из официантов
                             </Typography>
                             <div className='main-page-paper-actions'>
-                                <Button variant='raised'>
-                                    Официанты
-                                </Button>
-                                <Button variant='raised'>
-                                    ККТ
-                                </Button>
+                                <div className='main-page-button-wrapper'>
+                                    <Button variant='raised'
+                                            color='primary'>
+                                        Официанты
+                                    </Button>
+                                    <Button variant='raised'
+                                            color='primary'>
+                                        ККТ
+                                    </Button>
+                                </div>
+                            </div>
+                            <Divider/>
+                            <div className='main-page-card-block-wrapper'>
+                                {this.state.cardNumber === null &&
+                                <Typography component='p' style={{fontSize: '18px'}}>
+                                    Вы еще не ввели номер карты, безналичные чаевые пока недоступны!
+                                </Typography>}
+                                <div className='main-page-card-input-wrapper'>
+                                    <TextField className='main-page-card-number'
+                                               value={this.state.cardNumber}
+                                               onChange={event => this.setState({cardNumber: event.target.value})}
+                                               label='Номер карты'/>
+                                    <div className='main-page-save-card-button-wrapper'>
+                                        <Button variant='outlined'
+                                                onClick={() => this.saveCard()}>
+                                            Сохранить
+                                        </Button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </Paper>
